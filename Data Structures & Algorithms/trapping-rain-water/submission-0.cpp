4b9a8b49a79c4d03acae3483d8ce491101f50939
Thu Chau <100187506+ThuChaulge@users.noreply.github.com>
2026-07-13 19:37:30 +0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n <= 2) return 0;
+        vector<int> left_max(n, 0);
+        vector<int> right_max(n, 0);
+        for (int i = 0; i < n-1; i++) {
+            left_max[i+1] = max(left_max[i], height[i]);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            right_max[i-1] = max(right_max[i], height[i]);
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = min (left_max[i], right_max[i]) - height[i];
+            if (temp > 0) result += temp;
+            cout << result << endl;
+        }
+        return result;
+    }
+};
