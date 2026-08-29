976b9ef91e841576204e910f1269ae892e5cfde6
Thu Chau <100187506+ThuChaulge@users.noreply.github.com>
2026-08-29 09:24:48 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int result = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > minPrice) {
+                result = max(result, prices[i] - minPrice);
+            }
+            minPrice = min(minPrice, prices[i]);
+        }
+        return result;
+    }
+};
